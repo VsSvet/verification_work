15364d2be27578e14f6264885d08f34df83b5f37
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-02-24 09:51:31 +0300

    add a solution

--- array_of_strings.py
+++ array_of_strings.py
@@ -1,3 +1,8 @@
 array_of_strings = ["hello", "2", "world", ":-)"]
 str_meet_the_requirements = []
 index = 0
+while index != len(array_of_strings):
+    if len(array_of_strings[index]) <= 3:
+        str_meet_the_requirements.append(array_of_strings[index])
+    index += 1
+print(str_meet_the_requirements)
